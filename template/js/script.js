f84810631a77a9060244cfb66d2c220651004ad8
Mojtaba Esparham <mojtaba.esparham@gmail.com>
2022-05-16 16:23:01 +0200

    Initial commit

--- template/js/script.js
+++ template/js/script.js
@@ -1,5 +1,6 @@
 const container = document.getElementsByClassName('container')[0];
 const sideMenu = document.getElementById('side-menu');
+const newTwittModal = document.getElementById('new-twitt-modal');
 
 function showSideMenu() {
   sideMenu.style.left = '0';
@@ -9,4 +10,12 @@ function showSideMenu() {
 function hideSideMenu() {
   sideMenu.style.left = '-280px';
   container.style.filter = 'none';
+}
+
+function showNewTwitModal() {
+  newTwittModal.style.top = '0%';
+}
+
+function hideNewTwitModal() {
+  newTwittModal.style.top = '100%';
 }
