9451d69dfa8618a343bfec78f57c5a2b93001e81
Mojtaba Esparham <mojtaba.esparham@gmail.com>
2022-05-17 20:13:37 +0200

    Add user image

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
 const authCheck = require('../middleware/auth.middleware');
+const fileUpload = require('../middleware/fileUpload.middleware');
 
 //Signup
-router.post('/signup', userController.signUp);
+router.post('/signup', fileUpload.single('image'), userController.signUp);
 //Login
 router.post('/login', userController.login);
 //Reset Password
