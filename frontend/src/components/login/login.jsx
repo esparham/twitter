ac96161b3201e1a362f6e0d9f7dbdc61220c3386
Mojtaba Esparham <mojtaba.esparham@gmail.com>
2022-05-18 15:50:40 +0200

    Handle errors

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import useInput from '../../hooks/useInput';
+import ReactDom from 'react-dom';
 import useHttp from '../../hooks/useHttp';
 import config from '../../appconfig.json';
 import { useDispatch } from 'react-redux';
@@ -8,8 +9,11 @@ import classes from './login.module.css';
 import Spinner from '../ui/Spinner/spinner';
 import { Link, useNavigate } from 'react-router-dom';
 import jwt from 'jsonwebtoken';
+import Message from '../modal/message';
 
 const Login = () => {
+  const [showModal, setShowModal] = useState(false);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -39,7 +43,13 @@ const Login = () => {
 
   const { isLoading, error, sendRequest } = useHttp(setTokenAndNavigate);
 
-  //TODO handle errors
+  useEffect(() => {
+    if (error !== null) {
+      setShowModal(true);
+    } else {
+      setShowModal(false);
+    }
+  }, [error]);
 
   const loginRequestConfig = {
     url: `${config.api}user/login`,
@@ -71,45 +81,64 @@ const Login = () => {
   useEffect(() => {
     checkToken();
   }, [checkToken]);
+  const modalRoot = document.getElementById('modal');
 
   return (
-    <div className={classes.container}>
-      <i className="fa-brands fa-twitter"></i>
-      <h1>Twitter</h1>
-      <h2>Login</h2>
+    <React.Fragment>
+      {ReactDom.createPortal(
+        <Message
+          header="Error!"
+          text={error && error.message}
+          show={showModal}
+          buttons={[
+            {
+              text: 'Retry',
+              type: 'success',
+              action: () => {
+                setShowModal(false);
+              },
+            },
+          ]}
+        />,
+        modalRoot
+      )}
 
-      <div className={classes.form}>
-        <form onSubmit={handleLogin}>
-          <label htmlFor="email">Email</label>
-          <input
-            className={emailHasError ? classes.error : classes.valid}
-            type="email"
-            id="email"
-            autoComplete="email"
-            onBlur={emailInputBlurHandler}
-            value={emailValue}
-            onChange={emailValueChangeHandler}
-          />
-          <label htmlFor="email">Password</label>
-          <input
-            className={passwordHasError ? classes.error : classes.valid}
-            type="password"
-            id="password"
-            autoComplete="password"
-            onBlur={passwordInputBlurHandler}
-            value={passwordValue}
-            onChange={passwordValueChangeHandler}
-          />
-          <button disabled={!formIsValid}>
-            {isLoading ? <Spinner /> : 'Login'}
-          </button>
-        </form>
-        {/* TODO Change links*/}
-        <a href="">Forgot password?</a>
+      <div className={classes.container}>
+        <i className="fa-brands fa-twitter"></i>
+        <h1>Twitter</h1>
+        <h2>Login</h2>
 
-        <Link to="/register">Do not have an account?</Link>
+        <div className={classes.form}>
+          <form onSubmit={handleLogin}>
+            <label htmlFor="email">Email</label>
+            <input
+              className={emailHasError ? classes.error : classes.valid}
+              type="email"
+              id="email"
+              autoComplete="email"
+              onBlur={emailInputBlurHandler}
+              value={emailValue}
+              onChange={emailValueChangeHandler}
+            />
+            <label htmlFor="email">Password</label>
+            <input
+              className={passwordHasError ? classes.error : classes.valid}
+              type="password"
+              id="password"
+              autoComplete="password"
+              onBlur={passwordInputBlurHandler}
+              value={passwordValue}
+              onChange={passwordValueChangeHandler}
+            />
+            <button disabled={!formIsValid}>
+              {isLoading ? <Spinner /> : 'Login'}
+            </button>
+          </form>
+          <Link to="/forgotPassword">Forgot password?</Link>
+          <Link to="/register">Do not have an account?</Link>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
