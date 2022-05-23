335ab253d75754ce45cd585a3c4a9a58c2a4ac8f
Mojtaba Esparham <mojtaba.esparham@gmail.com>
2022-05-23 19:28:31 +0200

    Change logout logic

--- frontend/src/components/logout/logout.jsx
+++ frontend/src/components/logout/logout.jsx
@@ -1,11 +1,17 @@
 import { useDispatch } from 'react-redux';
 import { userActions } from '../../store/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const Logout = () => {
   const dispatch = useDispatch();
-  dispatch(userActions.setUserLogout());
+  const navigate = useNavigate();
 
-  localStorage.removeItem('token');
+  useEffect(() => {
+    dispatch(userActions.setUserLogout());
+    localStorage.clear();
+    navigate('/login');
+  }, [dispatch, navigate]);
 };
 
 export default Logout;
